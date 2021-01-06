9d56963ccdc65420c467cdf837d7c1ac64b37741
rajdeo prasad <raj@gmail.com>
2021-01-06 09:34:26 +0530

    first commit

---
+++ filehaimerapronahi.py
@@ -0,0 +1,33 @@
+
+#C:\Users\RAJDEO\Downloads\DOC\1_PLACEMENTcourseIMP
+Courses = ["Complete Machine Learning and Data Science Zero to Mastery",
+          "Complete Python Developer in 2021 Zero to Mastery ",
+          "LoveBabbarBOOKS",
+          "MERN Stack React Node Ecommerce from Scratch to Deployment",
+          "React Redux Ecommerce_Master MERN Stack Web Development",
+          "RESUME",
+          "SQL and PostgreSQL The Complete Developer Guide",
+          "TheCompletePython & PostgreSQLDeveloperCourse",
+          "Udemy - JavaScriptWebProjects20ProjectstoBuildPorfolio",
+          "zBooksIT"]
+#D:\Golu
+Courses2 = ["HTML, CSS, JavaScript - Build 6 Creative Projects",
+          "JavaScript The Advanced Concepts ",
+          "LoveBabbarBOOKS",
+          "Learn JavaScript Full-Stack from ScratchUdemy",
+          "MERN Stack Front To Back Full Stack React, Redux & Node.js",
+          "Udemy - Oracle SQL  Become a Certified SQL Developer From Scratch!"]
+#E:\1_DOC\1_RAJDEO\1_LearnIT
+Courses3 = ["Complete E-Commerce Course - Java,Spring,Hibernate and MySQL",
+          "JAVASCRIPT_Udemy - JavaScript Algorithms and Data Structures Masterclass ",
+          "Master the Coding Interview Big Tech (FAANG) Interviews",
+          "REACT",
+          "The Complete Web Developer in 2021 Zero to Mastery",
+          "UDEMYlearning"]
+
+for x in range(len(Courses)): 
+    print(Courses[x])
+for x in range(len(Courses2)): 
+    print(Courses2[x])
+for x in range(len(Courses3)): 
+    print(Courses3[x])
